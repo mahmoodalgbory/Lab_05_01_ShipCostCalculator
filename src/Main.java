03fcf26480580c8ba88aa2761aca3c7d0f513de2
mahmoodalgbory <algbormd@mail.uc.edu>
2022-09-22 14:26:29 -0400

    did it

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        int itemPrice = 69;
+        double totalPrice = itemPrice * 1.02;
+
+
+        if (itemPrice >= 100)
+        {
+            System.out.println("Congratulations you qualify for free shipping your total is " + itemPrice);
+        }
+        else
+        {
+            System.out.print("You do not qualify for free shipping your total is " + totalPrice);
+        }
+
     }
 }
